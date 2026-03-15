0d5a4e76f237de8f3eaea72d0cc9be8567bc98eb
Riddhi Paul <riddhipaul8240@gmail.com>
2026-03-15 19:21:14 +0530

    Added backend headless=True in scrape_jobs.py

--- LinkedInJobsAutomatior/Backend/scrape_jobs.py
+++ LinkedInJobsAutomatior/Backend/scrape_jobs.py
@@ -40,7 +40,7 @@ def scrape_jobs(keyword="GenAI Developer",
             base_url += f"&f_TPR={time_filter}"
 
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
 
         page.goto(base_url)
